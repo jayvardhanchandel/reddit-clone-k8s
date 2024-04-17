fd58f1180489ad9f008cc4e648dd44626d8fdb8e
JAY  VARDHAN CHANDEL <158720327+jayvardhanchandel@users.noreply.github.com>
2024-04-17 10:32:46 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jay-bucket-0604" # Replace with your actual S3 bucket name
+    bucket = "jay-bucket-06041991" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
